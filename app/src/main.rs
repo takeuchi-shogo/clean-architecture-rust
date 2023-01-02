668e07b9d602416b4f69b4879514105b9e4ea2b5
ShogoTakeuchi <smth.1.23@icloud.com>
2023-01-02 11:45:12 +0900

    DieselでDocker内のMySQLへ接続確認

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,8 +3,8 @@ use clean_architecture_rust::infrastructure;
 
 pub fn main() {
 
-	let _ = infrastructure::config::AppConfig::new_config();
-	// let _ = infrastructure::db::new_database(&config);
+	let config = infrastructure::config::AppConfig::new_config();
+	let _ = infrastructure::db::new_database(&config);
 
 	infrastructure::routing::run();
 }
